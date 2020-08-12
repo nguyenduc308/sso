a2bb7d3aedcd18e3168028a5649905dca03db4d9
duc <duc@gmail.com>
2020-08-12 21:24:48 +0700

    add

--- services/src/datasource/typeorm.config.ts
+++ services/src/datasource/typeorm.config.ts
@@ -5,7 +5,7 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
     host: 'localhost',
     port: 3306,
     username: 'root',
-    password: 'Duc@123456',
+    password: 'abc123',
     database: 'sql_medium',
     entities: ["dist/**/*.entity.{ts,js}"],
     synchronize: true
